efa95ca826994705fe85712db8525146525cee5a
Daniel900 <Daniel900@users.noreply.github.com>
2026-09-02 23:34:07 +0200

    More custom icons

--- src/TwitchSubscriberPictures/TrayIconController.cs
+++ src/TwitchSubscriberPictures/TrayIconController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows;
 using System.Windows.Controls;
 using Hardcodet.Wpf.TaskbarNotification;
@@ -17,6 +18,7 @@ public sealed class TrayIconController : IDisposable
     private readonly Action _showWindow;
     private readonly Action _closeImmediately;
     private TaskbarIcon? _taskbarIcon;
+    private Icon? _currentIcon;
 
     public TrayIconController(Action showWindow, Action closeImmediately)
     {
@@ -31,10 +33,12 @@ public sealed class TrayIconController : IDisposable
             return;
         }
 
+        _currentIcon = CreateStatusIcon(TwitchConnectionStatus.Disconnected);
+
         _taskbarIcon = new TaskbarIcon
         {
             ToolTipText = "Twitch Subscriber Pictures",
-            Icon = SystemIcons.Application,
+            Icon = _currentIcon,
             ContextMenu = BuildContextMenu(),
             Visibility = Visibility.Visible
         };
@@ -49,13 +53,57 @@ public sealed class TrayIconController : IDisposable
             return;
         }
 
-        (_taskbarIcon.Icon, _taskbarIcon.ToolTipText) = status switch
+        var newIcon = CreateStatusIcon(status);
+        var previousIcon = _currentIcon;
+        _currentIcon = newIcon;
+        _taskbarIcon.Icon = newIcon;
+        previousIcon?.Dispose();
+
+        _taskbarIcon.ToolTipText = status switch
         {
-            TwitchConnectionStatus.EventSubLive => (SystemIcons.Shield, "Twitch Subscriber Pictures - Connected, EventSub live"),
-            TwitchConnectionStatus.Connected => (SystemIcons.Information, "Twitch Subscriber Pictures - Connected"),
-            TwitchConnectionStatus.TokenInvalid => (SystemIcons.Warning, "Twitch Subscriber Pictures - Token invalid"),
-            _ => (SystemIcons.Application, "Twitch Subscriber Pictures - Disconnected")
+            TwitchConnectionStatus.EventSubLive => "Twitch Subscriber Pictures - Connected, EventSub live",
+            TwitchConnectionStatus.Connected => "Twitch Subscriber Pictures - Connected",
+            TwitchConnectionStatus.TokenInvalid => "Twitch Subscriber Pictures - Token invalid",
+            _ => "Twitch Subscriber Pictures - Disconnected"
         };
+    }
+
+    private static Icon CreateStatusIcon(TwitchConnectionStatus status)
+    {
+        var color = status switch
+        {
+            TwitchConnectionStatus.EventSubLive => Color.FromArgb(46, 160, 67),
+            TwitchConnectionStatus.Connected => Color.FromArgb(180, 140, 30),
+            TwitchConnectionStatus.TokenInvalid => Color.FromArgb(200, 60, 60),
+            _ => Color.FromArgb(120, 120, 120)
+        };
+
+        var glyph = status switch
+        {
+            TwitchConnectionStatus.EventSubLive => "T",
+            TwitchConnectionStatus.Connected => "T",
+            TwitchConnectionStatus.TokenInvalid => "!",
+            _ => "-"
+        };
+
+        using var bitmap = new Bitmap(16, 16);
+        using var graphics = Graphics.FromImage(bitmap);
+        graphics.SmoothingMode = SmoothingMode.AntiAlias;
+        graphics.Clear(Color.Transparent);
+
+        using var backgroundBrush = new SolidBrush(color);
+        graphics.FillEllipse(backgroundBrush, 1, 1, 14, 14);
+
+        using var font = new Font("Segoe UI", 8f, System.Drawing.FontStyle.Bold, GraphicsUnit.Pixel);
+        using var textBrush = new SolidBrush(Color.White);
+        using var format = new StringFormat
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center
+        };
+
+        graphics.DrawString(glyph, font, textBrush, new RectangleF(1, 1, 14, 14), format);
+        return Icon.FromHandle(bitmap.GetHicon());
     }
 
     private ContextMenu BuildContextMenu()
@@ -79,6 +127,8 @@ public sealed class TrayIconController : IDisposable
 
     public void Dispose()
     {
+        _currentIcon?.Dispose();
+        _currentIcon = null;
         _taskbarIcon?.Dispose();
         _taskbarIcon = null;
     }
